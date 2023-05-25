a6527709c9166044e02a2611cdc7405271bb03bb
DengBoYuan <dengboyuan.work@gmail.com>
2023-05-25 14:39:38 +0800

    Leetcode98

---
+++ src/com/leetcode/EightoneToHundred/Leetcode98.java
@@ -0,0 +1,31 @@
+package com.leetcode.EightoneToHundred;
+
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+
+/*
+    思路：中序遍历，判断是否为升序
+ */
+public class Leetcode98 {
+    public boolean isValidBST(TreeNode root) {
+        Deque<TreeNode> stack = new LinkedList<TreeNode>();
+        double inorder = -Double.MAX_VALUE;
+
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            // 如果中序遍历得到的节点的值小于等于前一个 inorder，说明不是二叉搜索树
+            if (root.val <= inorder) {
+                return false;
+            }
+            inorder = root.val;
+            root = root.right;
+        }
+        return true;
+    }
+}
