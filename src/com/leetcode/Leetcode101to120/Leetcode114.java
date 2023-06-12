c1e8cae9f398794bb0f579aa330fdb46c52c0c08
DengBoYuan <dengboyuan.work@gmail.com>
2023-06-12 19:49:54 +0800

    leetcode114

---
+++ src/com/leetcode/Leetcode101to120/Leetcode114.java
@@ -0,0 +1,37 @@
+package com.leetcode.Leetcode101to120;
+
+import com.leetcode.tool.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Leetcode114 {
+    Queue<TreeNode> queue = new LinkedList<>();
+    public void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        } else {
+            preOrder(root);
+        }
+        TreeNode preNode = queue.poll();
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            preNode.right = node;
+            preNode = preNode.right;
+        }
+
+    }
+    public void preOrder(TreeNode root) {
+        queue.offer(root);
+        if (root.left != null) {
+            TreeNode left = root.left;
+            root.left = null;
+            preOrder(left);
+        }
+        if (root.right != null) {
+            TreeNode right = root.right;
+            root.right = null;
+            preOrder(right);
+        }
+    }
+}
